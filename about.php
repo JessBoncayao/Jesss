1a9f3e329b2995a004d96cf020209bc719f3801e
JessBoncayao <162594601+JessBoncayao@users.noreply.github.com>
2024-03-07 22:36:32 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="script.js"></script>
+</head>
+
+<body>
+
+
+    <header class="header">
+        <a href="#home" class="logo"> <span></span></a>
+
+
+        <nav class="navbar">
+            <a href="home.php" class="active">Home</a>
+            <a href=about.php>About</a>
+            <a href=skills.php>Skills</a>
+            <a href=portfolio.php>Portfolio</a>
+            <a href=comments.php>Comments</a>
+            <a href=logout.php>Logout</a>
+        </nav>
+    </header>
+
+    <section class="about" id="about">
+
+        <div class="heading4">
+            <h2>About Me</h2>
+
+            <span>Introduction</span>
+        </div>
+
+        <div class="about-container">
+            <div class="about-img">
+                <img src="pictures/js2.jpeg" id="pictures" alt="">
+            </div>
+
+            <div class="about-text">
+                <p>Hello, I'm Jess Boncayao, an enthusiastic student with a deep interest in coding and technology. I'm
+                    driven by the endless possibilities that coding offers and I'm always eager to dive into new
+                    projects and challenges. Whether it's building apps, solving algorithmic puzzles, or exploring
+                    cutting-edge technologies, I'm committed to honing my coding skills and pushing the boundaries of
+                    what I can create. I'm thrilled to be part of this dynamic community and can't wait to collaborate
+                    and learn from others.</p>
+
+                <div class="information">
+                    <div class="info-box">
+                        <i class='bx bx-user-circle'></i>
+                        <span>Jess Boncayao</span>
+                    </div>
+
+                    <div class="info-box">
+                        <i class='bx bx-phone'></i>
+                        <span>+63 912 345 6789</span>
+                    </div>
+
+                    <div class="info-box">
+                        <i class='bx bx-envelope'></i>
+                        <span>boncayaojess002@gmail.com</span>
+                    </div>
+
+                    <div class="info-box">
+                        <i class='bx bx-current-location'></i>
+                        <span>Bagong Silang Caloocan City </span>
+                    </div>
+
+                    <div class="info-box">
+                        <i class='bx bx-male-sign'></i>
+                        <span>Male </span>
+                    </div>
+
+                    <div class="info-box">
+                        <i class='bx bx-calendar'></i>
+                        <span>October 26, 2002 </span>
+                    </div>
+                </div>
+            </div>
+    </section>
+</body>
+
+</html>
